d98effa7683552aa0b5add8b55a38861fd96e57e
yeondori <yeonseo.jung0@gmail.com>
2023-11-23 19:19:09 +0900

    feat: 결과 화면 완성

--- lib/screen/screen_result.dart
+++ lib/screen/screen_result.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screen/screen_home.dart';
 import '../model/model_quiz.dart';
 
 class ResultScreen extends StatelessWidget {
@@ -37,7 +38,78 @@ class ResultScreen extends StatelessWidget {
               width: width * 0.85,
               height: height * 0.5,
               child: Column(
-                children: <Widget>[],
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(bottom: width * 0.048),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.deepPurple),
+                      color: Colors.white,
+                    ),
+                    width: width * 0.73,
+                    height: height * 0.25,
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(
+                              0, width * 0.048, 0, width * 0.012),
+                          child: Text(
+                            '수고하셨습니다!',
+                            style: TextStyle(
+                              fontSize: width * 0.055,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Text(
+                            '당신의 점수는',
+                          style: TextStyle(
+                            fontSize: width * 0.048,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Expanded(
+                            child: Container(),
+                        ),
+                        Text(
+                          score.toString() + '/' + quizs.length.toString(),
+                          style: TextStyle(
+                            fontSize: width * 0.21,
+                            color: Colors.red,
+                          ),
+                        ),
+                        Padding(
+                            padding: EdgeInsets.all(width * 0.012),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: width * 0.048),
+                    child: ButtonTheme(
+                      minWidth: width * 0.73,
+                      height: height * 0.05,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: ElevatedButton(onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) {
+                          return HomeScreen();
+                        }));
+                      },
+                        child: Text('홈으로 돌아가기', style: TextStyle(color: Colors.black),),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
