97516f5f2c7828c2781209246c572ba7a0655491
yeondori <yeonseo.jung0@gmail.com>
2023-11-23 17:45:59 +0900

    refactor: rename(candidate -> candidates)

--- lib/model/model_quiz.dart
+++ lib/model/model_quiz.dart
@@ -1,12 +1,12 @@
 class Quiz {
   String title;
-  List<String> candidate;
+  List<String> candidates;
   int answer;
 
-  Quiz({required this.title, required this.candidate, required this.answer});
+  Quiz({required this.title, required this.candidates, required this.answer});
 
   Quiz.fromMap(Map<String, dynamic> map)
       : title = map['title'],
-        candidate= map['candidates'],
+        candidates= map['candidates'],
         answer = map['answer'];
 }
