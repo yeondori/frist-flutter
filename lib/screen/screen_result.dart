999ba67370d62647fa0974886860b23541913456
yeondori <yeonseo.jung0@gmail.com>
2023-11-23 19:03:02 +0900

    feat: 결과 화면 추가

---
+++ lib/screen/screen_result.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../model/model_quiz.dart';
+
+class ResultScreen extends StatefulWidget {
+  List<int> answers;
+  List<Quiz> quizs;
+  ResultScreen({required this.answers, required this.quizs});
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    int score = 0;
+    for (int i = 0; i < quizs.length; i++) {
+      if (quizs[i].answer == answers[i]) {
+        score += 1;
+      }
+    }
+
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('My Quiz APP'),
+            backgroundColor: Colors.deepPurple,
+            leading: Container(),
+          ),
+          body: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.deepPurple),
+                color: Colors.deepPurple,
+              ),
+              width: width * 0.85,
+              height: height * 0.5,
+              child: Column(
+                children: <Widget>[],
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+}
